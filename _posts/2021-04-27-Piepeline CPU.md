9efae187ab182d308fa05d9b23448b32ac1029eb
kyungphilDev <phill0114@postech.ac.kr>
2021-04-28 16:25:31 +0900

    google console

--- _posts/2021-04-27-Piepeline CPU.md
+++ _posts/2021-04-27-Piepeline CPU.md
@@ -6,7 +6,7 @@ category: category
 tags: [computer architecture, computer science, pipeline, cpu]
 ---
 
-Pipeline CPU를 design & implement 하기 전에, Pipeline CPU의 특성들을 먼저 충분히 이해하는 것이 중요할 것 같습니다.  
+Pipeline CPU를 design & implement 하기 전에, Pipeline CPU의 특성들을 먼저 충분히 이해해봅시다.  
 Pipeline CPU는 이전에 배웠던 Single-cycle CPU, Multi-cycle CPU 에 비해서 같은 cycle 시간 동안 더 많은 instruction을 수행할 수 있습니다.
 
 어떠한 특성들 때문에 이러한 효율이 가능한 것일까요?  
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Pipeline CPU- Data Hazard",
-        "excerpt":"Pipeline CPU를 design &amp; implement 하기 전에, Pipeline CPU의 특성들을 먼저 충분히 이해하는 것이 중요할 것 같습니다. Pipeline CPU는 이전에 배웠던 Single-cycle CPU, Multi-cycle CPU 에 비해서 같은 cycle 시간 동안 더 많은 instruction을 수행할 수 있습니다. 어떠한 특성들 때문에 이러한 효율이 가능한 것일까요? 그리고 구현 상에 어떠한 점을 주의해야...","categories": ["category"],
+        "excerpt":"Pipeline CPU를 design &amp; implement 하기 전에, Pipeline CPU의 특성들을 먼저 충분히 이해해봅시다. Pipeline CPU는 이전에 배웠던 Single-cycle CPU, Multi-cycle CPU 에 비해서 같은 cycle 시간 동안 더 많은 instruction을 수행할 수 있습니다. 어떠한 특성들 때문에 이러한 효율이 가능한 것일까요? 그리고 구현 상에 어떠한 점을 주의해야 할까요? 세가지 주요 사항...","categories": ["category"],
         "tags": ["computer architecture","computer science","pipeline","cpu"],
         "url": "/category/Piepeline-CPU/",
         "teaser": null
