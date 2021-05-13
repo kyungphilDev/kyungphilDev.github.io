cf8fc01cb2533283fde7fc924ed3c7ea020daf8f
kyungphilDev <phill0114@postech.ac.kr>
2021-05-13 16:19:56 +0900

    new post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,13 +1,19 @@
 var store = [{
-        "title": "Pipeline CPU- Data Hazard",
-        "excerpt":"Pipeline CPU를 design &amp; implement 하기 전에, Pipeline CPU의 특성들을 먼저 충분히 이해해봅시다. Pipeline CPU는 이전에 배웠던 Single-cycle CPU, Multi-cycle CPU 에 비해서 같은 cycle 시간 동안 더 많은 instruction을 수행할 수 있습니다. 어떠한 특성들 때문에 이러한 효율이 가능한 것일까요? 그리고 구현 상에 어떠한 점을 주의해야 할까요? 세가지 주요 사항...","categories": ["category"],
+        "title": "Pipeline CPU(2)- Data Hazard",
+        "excerpt":"Pipeline CPU를 design &amp; implement 하기 전에, Pipeline CPU의 특성들을 먼저 충분히 이해해봅시다. 어떠한 특성들로 인해 Pipeline이 가장 빠르게 작동이 가능한 것일까요? 세가지 주요 사항 Data Hazard Control Hazard Branch Prediction 이번 Post에서는 위의 세가지 주요 특징들 중에서도 1. Data Hazard 를 해결하는 방법에 대해 구체적으로 알아 봅니다. RAW Hazard...","categories": ["Computer-Architecture"],
         "tags": ["computer architecture","computer science","pipeline","cpu"],
-        "url": "/category/Piepeline-CPU/",
+        "url": "/computer-architecture/DataHazard/",
         "teaser": null
       },{
         "title": "React Study(1) 정리",
-        "excerpt":"Component-Driven User Interfaces A Client-side JavaScript library All aobut building modern, reactive user interfaces for the web Building Single-Page-Applications (SPAs) 배워야할 개념들 Components &amp; Building UIs Working with Events &amp; Data: “props” and “state” Styling React Apps &amp; Components React Hooks React Redux Http Requests &amp; Custom Hooks Routing, Deployment,...","categories": ["category"],
+        "excerpt":"Component-Driven User Interfaces A Client-side JavaScript library All aobut building modern, reactive user interfaces for the web Building Single-Page-Applications (SPAs) 배워야할 개념들 Components &amp; Building UIs Working with Events &amp; Data: “props” and “state” Styling React Apps &amp; Components React Hooks React Redux Http Requests &amp; Custom Hooks Routing, Deployment,...","categories": ["React"],
         "tags": ["React","JS","Web Framework"],
-        "url": "/category/React-Study/",
+        "url": "/react/ReactStudy1/",
+        "teaser": null
+      },{
+        "title": "Pipeline CPU(1) <br> Single_Cycle_CPU VS Multi_Cycle_CPU",
+        "excerpt":"Pipeline CPU를 design &amp; implement 하기 위해서는 Single-Cycle과 Multi-Cycle CPU의 특성들을 이해하는 것이 필요합니다. 어떠한 특성으로 인해 Pipeline-CPU가 가장 빠르게 작동할 수 있을까요? Single-Cycle CPU 의 비효율성 Instruction 마다의 특성을 고려하지 못한 Single-Cycle-CPU Single-Cycle CPU 의 경우에는 새로운 Instruction이 입력되고 다음 Instruction이 입력될 때까지 Single cycle time이 소요됩니다. 하지만 여기서...","categories": ["Computer-Architecture"],
+        "tags": ["computer architecture","computer science","pipeline","cpu"],
+        "url": "/computer-architecture/pipeline1/",
         "teaser": null
       }]
