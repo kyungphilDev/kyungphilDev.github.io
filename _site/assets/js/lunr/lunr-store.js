b349f71f462ef78a2c8b1b4004b209b9a46c6074
kyungphilDev <phill0114@postech.ac.kr>
2021-05-13 13:45:49 +0900

    setting

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,10 +1,4 @@
 var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["category"],
-        "tags": ["jekyll","welcome"],
-        "url": "/category/welcome-to-jekyll/",
-        "teaser": null
-      },{
         "title": "Pipeline CPU- Data Hazard",
         "excerpt":"Pipeline CPU를 design &amp; implement 하기 전에, Pipeline CPU의 특성들을 먼저 충분히 이해해봅시다. Pipeline CPU는 이전에 배웠던 Single-cycle CPU, Multi-cycle CPU 에 비해서 같은 cycle 시간 동안 더 많은 instruction을 수행할 수 있습니다. 어떠한 특성들 때문에 이러한 효율이 가능한 것일까요? 그리고 구현 상에 어떠한 점을 주의해야 할까요? 세가지 주요 사항...","categories": ["category"],
         "tags": ["computer architecture","computer science","pipeline","cpu"],
