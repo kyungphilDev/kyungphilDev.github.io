df462fc770e1828eb236f47800ec8e9909bd594a
kyungphilDev <phill0114@postech.ac.kr>
2021-06-23 22:23:44 +0900

    new custom setting and update data-hazard post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,10 +1,4 @@
 var store = [{
-        "title": "Pipeline CPU(3)- Data Hazard",
-        "excerpt":"Pipeline-CPU 목차 Pipeline CPU(1) : Single-Cycle vs Multi-Cycle CPU Pipeline CPU(2) : Pipeline-CPU의 간략한 이해 Pipeline CPU(3) : Data Hazard 세가지 주요 사항 Data Hazard Control Hazard Branch Prediction 이번 Post에서는 위의 세가지 주요 특징들 중에서도 1. Data Hazard 를 해결하는 방법에 대해 구체적으로 알아 봅시다. Data Dependence Data Dependence의...","categories": ["Computer-Architecture"],
-        "tags": ["computer architecture","computer science","pipeline","cpu","RAW Hazrd","Branch Prediction","2-bit global"],
-        "url": "/computer-architecture/data-hazard/",
-        "teaser": null
-      },{
         "title": "React Study(1) 정리",
         "excerpt":"Component-Driven User Interfaces A Client-side JavaScript library All aobut building modern, reactive user interfaces for the web Building Single-Page-Applications (SPAs) 배워야할 개념들 Components &amp; Building UIs Working with Events &amp; Data: “props” and “state” Styling React Apps &amp; Components React Hooks React Redux Http Requests &amp; Custom Hooks Routing, Deployment,...","categories": ["React"],
         "tags": ["React","JS","Web Framework"],
@@ -21,5 +15,17 @@ var store = [{
         "excerpt":"Pipeline-CPU 목차 Pipeline CPU(1) : Single-Cycle vs Multi-Cycle CPU Pipeline CPU(2) : Pipeline-CPU의 간략한 이해 Pipeline CPU(3) : Data Hazard 앞서 Pipeline CPU를 design 해보기 위한 준비과정으로 Single-Cycle CPU와 Multi-Cycle CPU의 특성들을 먼저 살펴보았습니다. Pipeline CPU의 전체적 그림 Pipeline-CPU은 사실 Multi-Cycle-CPU에서 부터 출발합니다. 하지만, 어떤 면에서 더 우월한 점이 있을까요?...","categories": ["Computer-Architecture"],
         "tags": ["computer architecture","computer science","single cycle","single cycle cpu","multi cycle","multi cycle cpu","pipeline","cpu"],
         "url": "/computer-architecture/pipeline-2/",
+        "teaser": null
+      },{
+        "title": "Pipeline CPU(3)- Data Hazard",
+        "excerpt":"Pipeline-CPU 목차 Pipeline CPU(1) : Single-Cycle vs Multi-Cycle CPU Pipeline CPU(2) : Pipeline-CPU의 간략한 이해 Pipeline CPU(3) : Data Hazard(RAW) Pipeline CPU(4) : Data Forwarding 세가지 주요 사항 Data Hazard Control Hazard Branch Prediction 이번 Post에서는 Pipeline-CPU를 구현하기 위해 꼭 알아야할 위의 세가지 주요 사항들 중에서도 1. Data Hazard 를...","categories": ["Computer-Architecture"],
+        "tags": ["data hazard","RAW","pipeline","cpu","RAW Hazrd","Branch Prediction","2-bit global"],
+        "url": "/computer-architecture/pipeline-3/",
+        "teaser": null
+      },{
+        "title": "Pipeline CPU(4)- Data Forwarding",
+        "excerpt":"Pipeline-CPU 목차 Pipeline CPU(1) : Single-Cycle vs Multi-Cycle CPU Pipeline CPU(2) : Pipeline-CPU의 간략한 이해 Pipeline CPU(3) : Data Hazard(RAW) Pipeline CPU(4) : Data Forwarding STALL을 통한 Data Hazard 해결 RAW Hazard를 해결하는 가장 간단한 방법은 RAW가 발생한 두 Instruction간의 거리를 3보다 멀어지도록 두 Instruction 사이에 STALL을 추가해주는 방법이 있습니다....","categories": ["Computer-Architecture"],
+        "tags": ["data hazard","RAW","pipeline","cpu","RAW Hazrd","Branch Prediction","2-bit global"],
+        "url": "/computer-architecture/pipeline-4/",
         "teaser": null
       }]
